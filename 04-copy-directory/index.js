dd0278614991aed9ec72c357216e39fda7f865a2
Denis Romanovich <denis.romanovich10@gmail.com>
2023-05-08 16:27:42 +0300

    feat: add task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir(src, dest, callback) {
+  fs.mkdir(dest, { recursive: true }, (err) => {
+    if (err) return callback(err);
+
+    fs.readdir(src, { withFileTypes: true }, (err, entries) => {
+      if (err) return callback(err);
+
+      if (!entries.length) return callback();
+
+      for (let entry of entries) {
+        const srcPath = path.join(src, entry.name);
+        const destPath = path.join(dest, entry.name);
+
+        if (entry.isDirectory()) {
+          copyDir(srcPath, destPath, (err) => {
+            if (err) return callback(err);
+          });
+        } else {
+          fs.copyFile(srcPath, destPath, (err) => {
+            if (err) return callback(err);
+          });
+        }
+      }
+    });
+  });
+}
+
+copyDir(path.join(__dirname, "files"), path.join(__dirname, "files-copy"), (err) => {
+  if (err) throw err;
+});
